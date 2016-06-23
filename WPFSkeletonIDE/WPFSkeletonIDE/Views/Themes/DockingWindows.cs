bbdf38443d23f8c20307e0e059526d633e8cc15a
lriki <lriki.net@gmail.com>
2016-06-23 17:53:12 +0900

    add avalondock my themes.

---
+++ WPFSkeletonIDE/WPFSkeletonIDE/Views/Themes/DockingWindows.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Xceed.Wpf.AvalonDock.Themes;
+
+namespace WPFSkeletonIDE.Views.Themes
+{
+    public class DockingWindowsTheme : Theme
+    {
+        public override Uri GetResourceUri()
+        {
+            return new Uri(
+                "/Views/Themes/DockingWindows.xaml",
+                UriKind.Relative);
+        }
+    }
+}
