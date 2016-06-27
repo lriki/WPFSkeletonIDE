111c6a646a6d16d680a80253916d4ae55e26654e
lriki <lriki.net@gmail.com>
2016-06-27 20:31:31 +0900

    add contents.

--- WPFSkeletonIDE/WPFSkeletonIDE/Views/Panes/ErrorListPane.xaml.cs
+++ WPFSkeletonIDE/WPFSkeletonIDE/Views/Panes/ErrorListPane.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -23,6 +24,33 @@ namespace WPFSkeletonIDE.Views.Panes
         public ErrorListPane()
         {
             InitializeComponent();
+        }
+
+        class ErrorItem
+        {
+            public int Code { get; set; }
+            public string Description { get; set; }
+            public string Location { get; set; }
+        }
+
+        CollectionViewSource view = new CollectionViewSource();
+        ObservableCollection<ErrorItem> customers = new ObservableCollection<ErrorItem>();
+
+        private void UserControl_Loaded(object sender, RoutedEventArgs e)
+        {
+            int itemcount = 3;
+            for (int j = 0; j < itemcount; j++)
+            {
+                customers.Add(new ErrorItem()
+                {
+                    Code = j,
+                    Description = "Test",
+                    Location="Test.cs",
+                });
+            }
+
+            view.Source = customers;
+            this.listView1.DataContext = view;
         }
     }
 }
